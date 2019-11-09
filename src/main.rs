462e71cb8b75b9284f87550ae34e690e46b849f0
Andreas Schnebinger <andi.schnebinger@googlemail.com>
2019-11-09 15:30:42 +0100

    Add solutions for problem 1-7

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+mod problems;
+
+fn main() {
+    println!("Problem 1 solution: {}", problems::multiples(1000));
+    println!("Problem 2 solution: {}", problems::even_fibonacci(4000000));
+    println!(
+        "Problem 3 solution: {}",
+        problems::largest_prime_factor(600851475143)
+    );
+    println!(
+        "Problem 4 solution: {}",
+        problems::largest_palindrome_product(999 * 999, 100 * 100)
+    );
+    println!("Problem 5 solution: {}", problems::smallest_multiple(20));
+    println!(
+        "Problem 6 solution: {}",
+        problems::sum_square_difference(100)
+    );
+    println!("Problem 7 solution: {}", problems::nth_prime(10001));
+}
